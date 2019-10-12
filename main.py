081af93bc31b357ee60aaccf791e77402e1d0171
citrica <inma.rueda28@gmail.com>
2019-10-12 18:18:37 +0200

    Exercise 8.1

---
+++ main.py
@@ -0,0 +1,14 @@
+mood = "unhappy"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Let's go out for beers!")
+elif mood == "excited":
+    print("Oh my God! Me too!")
+elif mood == "relaxed":
+    print("You need to recharge your batteries.")
+else:
+    print("I don't recognize this mood.")
